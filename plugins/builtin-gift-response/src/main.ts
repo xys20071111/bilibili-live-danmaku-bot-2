29a6a0ad40eda75308a25fc67dcef171800ac7be
xys20071111 <xys20071111@hotmail.com>
2024-02-08 08:28:19 +0800

    礼物响应插件忘了禁用响应的部分了

--- plugins/builtin-gift-response/src/main.ts
+++ plugins/builtin-gift-response/src/main.ts
@@ -6,6 +6,24 @@ interface IMsg {
     data: any
 }
 
+interface IConfig {
+    cold_down_time?: number
+    free_gift_action?: boolean
+    disable_gift_action?: boolean
+    disable_super_chat_action?: boolean
+    disable_graud_action?: boolean
+    danmaku_template: {
+        gift: string
+        gift_total: string
+        guard: string
+        sc: string
+    }
+}
+
+interface IProgramConfig extends IConfig {
+    room_config: Record<string, IConfig>
+}
+
 // deno-lint-ignore no-explicit-any
 function formatTemplate(str: string, args: any): string {
     let result: string = str
@@ -22,87 +40,84 @@ async function main() {
     const decoder = new TextDecoder()
 
     const thanksColdDownSet = new Set<string>()
-    const config: {
-        cold_down_time?: number
-        free_gift_action?: boolean
-        disable_gift_action?: boolean
-        disable_super_chat_action?: boolean
-        disable_graud_action?: boolean
-        danmaku_template: {
-            gift: string
-            gift_total: string
-            guard: string
-            sc: string
-        }
-    } = JSON.parse(decoder.decode(await Deno.readFile(configFilePath)))
+    const config: IProgramConfig = JSON.parse(decoder.decode(await Deno.readFile(configFilePath)))
 
     const eventSocket = new WebSocket(`ws://127.0.0.1:${port}`)
 
     eventSocket.addEventListener('message', (event) => {
         const msg: IMsg = JSON.parse(event.data)
+        const room_config: IConfig = config.room_config[`${msg.room}`] || config
         if (msg.cmd === 'SEND_GIFT') {
-            if (thanksColdDownSet.has(msg.data.uname)) {
-                return
+            if (!config.disable_gift_action) {
+                if (thanksColdDownSet.has(msg.data.uname)) {
+                    return
+                }
+                if (room_config.free_gift_action || msg.data.super_gift_num > 0) {
+                    // 通过框架发送弹幕
+                    eventSocket.send(JSON.stringify({
+                        cmd: 'send',
+                        token,
+                        data: {
+                            room: msg.room,
+                            at: msg.data.uid,
+                            text: formatTemplate(config.danmaku_template.gift, {
+                                gift: msg.data.giftName,
+                                count: msg.data.super_gift_num
+                            })
+                        }
+                    }))
+                    thanksColdDownSet.add(msg.data.uname)
+                    setTimeout(() => { thanksColdDownSet.delete(msg.data.uname) }, config.cold_down_time)
+                }
             }
-            if (config.free_gift_action || msg.data.super_gift_num > 0) {
-                // 通过框架发送弹幕
+            return
+        }
+        if (msg.cmd === 'COMBO_SEND') {
+            if (!room_config.disable_gift_action) {
                 eventSocket.send(JSON.stringify({
                     cmd: 'send',
                     token,
                     data: {
                         room: msg.room,
                         at: msg.data.uid,
-                        text: formatTemplate(config.danmaku_template.gift, {
+                        text: formatTemplate(config.danmaku_template.gift_total, {
                             gift: msg.data.giftName,
                             count: msg.data.super_gift_num
                         })
                     }
                 }))
-                thanksColdDownSet.add(msg.data.uname)
-                setTimeout(() => { thanksColdDownSet.delete(msg.data.uname) }, config.cold_down_time)
             }
             return
         }
-        if (msg.cmd === 'COMBO_SEND') {
-            eventSocket.send(JSON.stringify({
-                cmd: 'send',
-                token,
-                data: {
-                    room: msg.room,
-                    at: msg.data.uid,
-                    text: formatTemplate(config.danmaku_template.gift_total, {
-                        gift: msg.data.giftName,
-                        count: msg.data.super_gift_num
-                    })
-                }
-            }))
-            return
-        }
         if (msg.cmd === 'GUARD_BUY') {
-            eventSocket.send(JSON.stringify({
-                cmd: 'send',
-                token,
-                data: {
-                    room: msg.room,
-                    at: msg.data.uid,
-                    text: formatTemplate(config.danmaku_template.guard, {
-                        gift: msg.data.gift_name,
-                        count: msg.data.num
-                    })
-                }
-            }))
+            if (!room_config.disable_graud_action) {
+                eventSocket.send(JSON.stringify({
+                    cmd: 'send',
+                    token,
+                    data: {
+                        room: msg.room,
+                        at: msg.data.uid,
+                        text: formatTemplate(config.danmaku_template.guard, {
+                            gift: msg.data.gift_name,
+                            count: msg.data.num
+                        })
+                    }
+                }))
+            }
             return
         }
         if (msg.cmd === 'SUPER_CHAT_MESSAGE') {
-            eventSocket.send(JSON.stringify({
-                cmd: 'send',
-                token,
-                data: {
-                    room: msg.room,
-                    at: msg.data.uid,
-                    text: formatTemplate(config.danmaku_template.sc, {})
-                }
-            }))
+            if (!room_config.disable_super_chat_action) {
+                eventSocket.send(JSON.stringify({
+                    cmd: 'send',
+                    token,
+                    data: {
+                        room: msg.room,
+                        at: msg.data.uid,
+                        text: formatTemplate(config.danmaku_template.sc, {})
+                    }
+                }))
+            }
             return
         }
     })
