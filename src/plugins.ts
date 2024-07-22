3f46be61180284bc16114a092d2743ad6604cb48
xys20071111 <xys20071111@hotmail.com>
2024-07-22 09:07:37 +0800

    一些杂七杂八的修改

--- src/plugins.ts
+++ src/plugins.ts
@@ -1,44 +1,52 @@
-import { config } from "./config.ts"
-import { printError } from "./utils/print_log.ts"
+import { config } from "./config.ts";
+import { printError } from "./utils/print_log.ts";
 
-const plugins: Map<Deno.Command, Deno.ChildProcess> = new Map()
+const plugins: Map<Deno.Command, Deno.ChildProcess> = new Map();
 
 export async function launchAllPlugins(token: string) {
-    const pluginsList = await Deno.readDir('./plugins')
-    for await (const plugin of pluginsList) {
-        if (plugin.name === '.gitkeep') {
-            continue
-        }
-        try {
-            const pluginCommand = new Deno.Command(`./plugins/${plugin.name}/main`, {
-                args: [`./plugins/${plugin.name}/config.json`, token, config.api.port.toString()]
-            })
-            const pluginProcess = pluginCommand.spawn()
-            pluginProcess.output().then(() => {
-                const pluginProcess = pluginCommand.spawn()
-                plugins.delete(pluginCommand)
-                plugins.set(pluginCommand, pluginProcess)
-            })
-            plugins.set(pluginCommand, pluginProcess)
-        } catch {
-            printError('主程序', `启动插件${plugin.name}失败`)
-        }
+  const pluginsList = Deno.readDir("./plugins");
+  for await (const plugin of pluginsList) {
+    if (plugin.name === ".gitkeep") {
+      continue;
     }
+    try {
+      const pluginCommand = new Deno.Command(`./plugins/${plugin.name}/main`, {
+        args: [
+          `./plugins/${plugin.name}/config.json`,
+          token,
+          config.api.port.toString(),
+        ],
+      });
+      const pluginProcess = pluginCommand.spawn();
+      pluginProcess.output().then(() => {
+        const pluginProcess = pluginCommand.spawn();
+        plugins.delete(pluginCommand);
+        plugins.set(pluginCommand, pluginProcess);
+      });
+      plugins.set(pluginCommand, pluginProcess);
+    } catch {
+      printError("主程序", `启动插件${plugin.name}失败`);
+    }
+  }
 }
 
 function stopAllPlugins() {
-    for (const pluginProcess of plugins.values()) {
-        pluginProcess.kill()
+  for (const pluginProcess of plugins.values()) {
+    try {
+      pluginProcess.kill();
+    } catch {
+      //Do nothing here
     }
-    Deno.exit()
+  }
+  Deno.exit();
 }
 
-Deno.addSignalListener('SIGTERM', () => {
-    stopAllPlugins()
-    Deno.exit()
-})
+Deno.addSignalListener("SIGTERM", () => {
+  stopAllPlugins();
+  Deno.exit();
+});
 
-Deno.addSignalListener('SIGINT', () => {
-    stopAllPlugins()
-    Deno.exit()
-})
+Deno.addSignalListener("SIGINT", () => {
+  stopAllPlugins();
+  Deno.exit();
+});
--- src/utils/print_log.ts
+++ src/utils/print_log.ts
@@ -1,13 +1,13 @@
-import { red, yellow } from 'fmt/colors.ts'
+import { red, yellow } from "fmt/colors";
 
 export function printError(tag: string, message: string) {
-    console.error(red('[ERROR]'), `[${tag}]`, red(message))
+  console.error(red("[ERROR]"), `[${tag}]`, red(message));
 }
 
 export function printWarning(tag: string, message: string) {
-    console.error(yellow('[WARNING]'), `[${tag}]`, yellow(message))
+  console.error(yellow("[WARNING]"), `[${tag}]`, yellow(message));
 }
 
 export function printLog(tag: string, message: string) {
-    console.log('[LOG]', `[${tag}]`, message)
+  console.log("[LOG]", `[${tag}]`, message);
 }
